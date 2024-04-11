23496ee910142b3659acc3564939cc62c98c3076
wackocontendor <65124419+wackocontendor@users.noreply.github.com>
2024-04-11 22:19:55 +0100

    Refactored code to use functions

--- livedepartureboard.py
+++ livedepartureboard.py
@@ -3,107 +3,96 @@ import requests
 import math
 import pandas as pd
 import os
-from functions import get_available_modes
-from functions import get_available_lines
 
-#These are the authentication keys to add to the end of any TfL API search request (it increases the number of calls per minute that you're allowed)
+# Create a list of all the available modes on the TfL network
+def get_all_modes(url_prefix, app_id, app_keys):
+    modes_response = requests.get(f'{url_prefix}Journey/Meta/Modes?app_id={app_id}&app_key={app_keys}')
+    modes_content = json.loads(modes_response.content)
+    modes_list = []
+    for mode in range(len(modes_content)):
+        if modes_content[mode]['isTflService'] == False:
+            continue
+        modes_list.append(modes_content[mode]['modeName'])
+    return modes_list
+
+# Create a list of all the lines for selected modes
+def get_lines_modes(search_modes, url_prefix, app_id, app_keys):
+    search_modes_parameter = ",".join(search_modes)
+    lines_response = requests.get(f'{url_prefix}line/mode/{search_modes_parameter}?app_id={app_id}&app_key={app_keys}')
+    lines_content = json.loads(lines_response.content)
+    lines_list = []
+    for line in range(len(lines_content)):
+        lines_list.append(lines_content[line]['id'])
+    return lines_list
+
+# Create a list of all the stations and IDs for selected modes
+def get_stations_modes(search_modes, url_prefix, app_id, app_keys):
+     None
+
+# Create a list of all the lines for selected station that are within the list of accepted lines
+def get_lines_station(station_id, valid_lines, url_prefix, app_id, app_keys):
+     lines_at_stop = []
+     lines_at_stop_response = requests.get(f'{url_prefix}Stoppoint/{station_id}?app_id={app_id}&app_key={app_keys}')
+     lines_at_stop_content = json.loads(lines_at_stop_response.content)
+     for line in range(len(lines_at_stop_content['lines'])):
+          if lines_at_stop_content['lines'][line]['id'] in valid_lines:
+               lines_at_stop.append(lines_at_stop_content['lines'][line]['id'])
+
+def get_arrivals_station(station_id, line_id, url_prefix, app_id, app_keys):
+     arrivals_dict = {'Platform': [],
+               'Destination': [],
+               'ETA': []}
+     arrivals_response = requests.get(f'{url_prefix}Line/{line_id}/Arrivals/{station_id}?app_id={app_id}&app_key={app_keys}')
+     arrivals_content = json.loads(arrivals_response.content)
+     for train in range(len(arrivals_content)):
+          arrivals_dict['Platform'].append(arrivals_content[train]['platformName'])
+          arrivals_dict['Destination'].append(arrivals_content[train]['destinationName'])
+          arrivals_dict['ETA'].append(math.floor(arrivals_content[train]['timeToStation']/60))
+     return arrivals_dict
+
 app_id = os.environ.get('TFL_API_ID')
 app_keys = os.environ.get('TFL_API_KEY')
+url_prefix = "https://api.tfl.gov.uk/"
+search_modes = ["dlr", "elizabeth-line", "overground", "tram" ,"tube"]
 
-#Set the list of TfL modes to search through
-search_modes = get_available_modes()
+# load a file into a dataframe that has a list of all the stations and their IDs
 
-#Valid list of lines on the modes
-valid_lines = get_available_lines()
+# get user to select a station and store the station id
 
-#Ask the user to search for a string
-search_term = input('\nEnter a search term to return a list of stations that match: ')
+chosen_station = "xx"
+chosen_station_id = "940GZZLUNGW"
 
-#URL to search
-response = requests.get(f'https://api.tfl.gov.uk/Stoppoint/Search/{search_term}?modes={search_modes}&app_id={app_id}&app_key={app_keys}')
+# get a list of all the valid lines for the given search modes
 
-#Load the output from the URL into a JSON
-data_content = json.loads(response.content)
+valid_lines = get_lines_modes(search_modes, url_prefix, app_id, app_keys)
 
-station_id = ''
-station_name = ''
+# get a list of all the valid lines at that stop
 
-#Ask the user to choose the number in the list that matches the station they were looking for, unless only one was returned
-if data_content['total'] == 0:
-     print("\nYour search term didn't return any results.")
+lines_at_station = get_lines_station(chosen_station_id, valid_lines, url_prefix, app_id, app_keys)
 
-elif data_content['total'] == 1:
-     station_id = data_content['matches'][0]['id']
-     station_name = data_content['matches'][0]['name']
+# get user to choose a line or prepopulate if only 1
 
-else:
-     #Tell the user how many stations matched their search
-     print('\nNumber of stations that match the search term:',data_content['total'],'\n')
+chosen_line_id = "jubilee"
 
-     #Loop through all the results of the search term and add each to a dictionary containing the station id and station name
-     station_dict = {}
+# get a list of arrivals for the chosen line and station
 
-     for x in range(1,int(len(data_content['matches']))+1):
-          station_dict.update({x: {'station_id': '', 'station_name': ''}})
-          station_dict[x]['station_id'] = data_content['matches'][x-1]['id']
-          station_dict[x]['station_name'] = data_content['matches'][x-1]['name']
-          print(x,station_dict[x]['station_name'])
+chosen_arrivals = get_arrivals_station(chosen_station_id, chosen_line_id, url_prefix, app_id, app_keys)
 
-     station_chosen = input('\nSelect a number from the options above: ')
+df = pd.DataFrame(chosen_arrivals)
 
-     station_id = station_dict[int(station_chosen)]['station_id']
-     station_name = station_dict[int(station_chosen)]['station_name']
-
-#Create a list of the lines at the stop
-
-lines_at_stop_response = requests.get(f'https://api.tfl.gov.uk/Stoppoint/{station_id}?app_id={app_id}&app_key={app_keys}')
-lines_at_stop_content = json.loads(lines_at_stop_response.content)
-lines_at_stop = []
-
-for line in range(len(lines_at_stop_content['lines'])):
-     if lines_at_stop_content['lines'][line]['id'] in valid_lines:
-          lines_at_stop.append(lines_at_stop_content['lines'][line]['id'])
-
-chosen_line = ''
-
-if len(lines_at_stop) == 1:
-     chosen_line = lines_at_stop[0]
-
-else:
-     print(f'\nPlease select the line you want to view the timetable for at {station_name}:\n')
-
-     for line in lines_at_stop:
-          print(lines_at_stop.index(line)+1,line)
-
-     chosen_line_input = input('\nSelect a number from the options above: ')
-     chosen_line = lines_at_stop[int(chosen_line_input)-1]
-
-#for HUBs, find the right naptan id
-if station_id[:3] == 'HUB':
-     #find the mode for the chosen line
-     mode_response = requests.get(f'https://api.tfl.gov.uk/Line/{chosen_line}')
-     mode_content = json.loads(mode_response.content)
-
-     line_mode = mode_content[0]['modeName']
-
-     #search through the children of the HUB to find the naptan id matching the mode
-     for child in range(len(lines_at_stop_content['children'])):
-          if line_mode in lines_at_stop_content['children'][child]['modes']:
-               station_id = lines_at_stop_content['children'][child]['naptanId']
-
-arrivals_response = requests.get(f'https://api.tfl.gov.uk/Line/{chosen_line}/Arrivals/{station_id}?app_id={app_id}&app_key={app_keys}')
-arrivals_content = json.loads(arrivals_response.content)
-
-arrivalsdict = {'Platform': [],
-                   'Destination': [],
-                   'ETA': []}
-
-for train in range(len(arrivals_content)):
-     arrivalsdict['Platform'].append(arrivals_content[train]['platformName'])
-     arrivalsdict['Destination'].append(arrivals_content[train]['destinationName'])
-     arrivalsdict['ETA'].append(math.floor(arrivals_content[train]['timeToStation']/60))
-
-df = pd.DataFrame(arrivalsdict)
 df.sort_values(by = ['Platform', 'ETA'], ascending = [True, True], inplace=True)
 
 print('\n',df.to_string(index=False))
+
+# What to do with HUBs - hopefully won't need this code if we can get the right ID in the first lookup
+# if station_id[:3] == 'HUB':
+#      #find the mode for the chosen line
+#      mode_response = requests.get(f'https://api.tfl.gov.uk/Line/{chosen_line}')
+#      mode_content = json.loads(mode_response.content)
+
+#      line_mode = mode_content[0]['modeName']
+
+#      #search through the children of the HUB to find the naptan id matching the mode
+#      for child in range(len(lines_at_stop_content['children'])):
+#           if line_mode in lines_at_stop_content['children'][child]['modes']:
+#                station_id = lines_at_stop_content['children'][child]['naptanId']
